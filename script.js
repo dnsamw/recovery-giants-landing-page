bae3111a8a2f935b93bc4d4c1ad61016be085177
Dileepa Samarawickrama <dnsam.mit@gmail.com>
2022-09-03 17:10:54 +0530

    type animation

--- script.js
+++ script.js
@@ -17,22 +17,22 @@ function changeBgColor(color) {
       desc: "Flat tyre change and wheel alignment. emergency breakdown service. Our experienced technicians are able to handle all types of vehicle issues. Nulla odio quos, nisi esse quae dicta dolorem commodi eveniet officiis autem cum et ciatis officiis doloribus molestias praesentium? Nulla odio quos, nisi animi architecto fugit veritatis aperiam quaerat",
     },
   };
-  const elTitle = document.querySelector("#title");
+  const elTitle = document.querySelector(".gig-title");
   const elDesc = document.querySelector("#description");
 
-  if (color === "#12af87") {
-    elTitle.innerHTML = data.tow.title;
-    elDesc.innerHTML = data.tow.desc;
-  }
-  if (color === "#0062be") {
-    elTitle.innerHTML = data.troubleShoot.title;
-    elDesc.innerHTML = data.troubleShoot.desc;
-  }
+  // if (color === "#12af87") {
+  //   elTitle.innerHTML = data.tow.title;
+  //   elDesc.innerHTML = data.tow.desc;
+  // }
+  // if (color === "#0062be") {
+  //   elTitle.innerHTML = data.troubleShoot.title;
+  //   elDesc.innerHTML = data.troubleShoot.desc;
+  // }
 
-  if (color === "#1e1e1e") {
-    elTitle.innerHTML = data.flatTyre.title;
-    elDesc.innerHTML = data.flatTyre.desc;
-  }
+  // if (color === "#1e1e1e") {
+  //   elTitle.innerHTML = data.flatTyre.title;
+  //   elDesc.innerHTML = data.flatTyre.desc;
+  // }
 
   console.log(color);
   const sec = document.querySelector(".sec");
