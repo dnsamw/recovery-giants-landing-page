ce7e45c9b14f70761622efe17121dba4a85903e6
Dileepa Samarawickrama <dnsam.mit@gmail.com>
2022-09-27 12:59:35 +0530

    mobile menu free quote form

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//hamburger
+//hamburger and Free quote button
 window.onload = function () {
   window.addEventListener("scroll", function (e) {
     if (window.pageYOffset > 100) {
@@ -12,6 +12,11 @@ window.onload = function () {
   const mobile_menu = document.querySelector("#toggleMenu");
   const menu_items = document.querySelectorAll(".menu-item");
 
+  //quote
+  const quote_btn = document.querySelector("#free-quote");
+  const quote_container = document.querySelector("#quote-container");
+  const form_close_btn = document.querySelector("#form-close");
+
   for (i of menu_items) {
     i.addEventListener("click", () => {
       mobile_menu.classList.toggle("is-active");
@@ -23,6 +28,57 @@ window.onload = function () {
     menu_btn.classList.toggle("is-active");
     mobile_menu.classList.toggle("is-active");
   });
+
+  quote_btn.addEventListener("click", function () {
+    quote_container.classList.toggle("is-active");
+  });
+
+  form_close_btn.addEventListener("click", function () {
+    quote_container.classList.toggle("is-active");
+  });
+
+  //forms
+  const form1 = document.getElementById("Form1");
+  const form2 = document.getElementById("Form2");
+  const form3 = document.getElementById("Form3");
+
+  //buttons
+  const next1 = document.getElementById("next1");
+  const next2 = document.getElementById("next2");
+  const back1 = document.getElementById("back1");
+  const back2 = document.getElementById("back2");
+  //const getBtn = document.getElementById("get-btn");
+
+  //progress
+  const progress = document.getElementById("progress");
+
+  // getBtn.onclick = function () {
+  //   var x = document.getElementById("container");
+  // };
+
+  next1.onclick = function () {
+    form1.style.left = "-450px";
+    form2.style.left = "40px";
+    progress.style.width = "240px";
+  };
+
+  next2.onclick = function () {
+    form2.style.left = "-450px";
+    form3.style.left = "40px";
+    progress.style.width = "360px";
+  };
+
+  back1.onclick = function () {
+    form1.style.left = "40px";
+    form2.style.left = "450px";
+    progress.style.width = "120px";
+  };
+
+  back2.onclick = function () {
+    form2.style.left = "40px";
+    form3.style.left = "450px";
+    progress.style.width = "240px";
+  };
 };
 
 //Testimonials Swiper
